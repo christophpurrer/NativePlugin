7fcf55a81eeedcdc82f8a69dc6dbc00e2e4db49e
Christoph Purrer <christophpurrer@gmail.com>
2023-07-25 22:56:12 -0700

    Initial Import

---
+++ PluginSource/source/ExamplePluginFunction.cpp
@@ -0,0 +1,12 @@
+//
+//  ExamplePluginFunction.cpp
+//  NativePlugin
+//
+//  Created by Christoph Purrer on 7/25/23.
+//
+
+#include "ExamplePluginFunction.hpp"
+
+extern "C" float ExamplePluginFunction() {
+    return 5.0F;
+}
---
+++ PluginSource/source/ExamplePluginFunction.hpp
@@ -0,0 +1,10 @@
+//
+//  ExamplePluginFunction.hpp
+//  NativePlugin
+//
+//  Created by Christoph Purrer on 7/25/23.
+//
+
+#pragma once
+
+extern "C" float ExamplePluginFunction();
